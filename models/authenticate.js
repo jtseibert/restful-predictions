1073d2e0482429b9780d3d76179b63708732a005
Jacob Seibert <12jtseibert@gmail.com>
2016-06-15 13:30:43 -0500

    test

--- models/authenticate.js
+++ models/authenticate.js
@@ -1,64 +1,87 @@
+// requires
+var https = require('https'),
+    http = require('http'),
+    oauth2 = require('simple-oauth2'),
+    bufferJson = require('buffer-json')
+
+/* For oauth2 authentication and token handling */
+
+    // Set the configuration settings
+var credentials = {
+        clientID: '3MVG9uudbyLbNPZMn2emQiwwmoqmcudnURvLui8uICaepT6Egs.LFsHRMAnD00FSog.OXsLKpODzE.jxi.Ffu',
+        clientSecret: '625133588109438640',
+        site: 'https://login.salesforce.com',
+        authorizationPath: '/services/oauth2/authorize',
+        tokenPath: '/services/oauth2/token',
+        revokePath: '/services/oauth2/revoke'
+    },
+    token,
+    options,
+    hostURL
+
+// Initialize the OAuth2 Library
+var oauth2 = oauth2(credentials)
+
+// Authorization uri definition
+var authorization_uri = oauth2.authCode.authorizeURL({
+        redirect_uri: 'https://hello-world360.herokuapp.com/callback',
+        scope: 'full'
+    })
 
 
-// /* For oauth2 authentication and token handling */
-// // Set the configuration settings
-// var credentials = {
-//         clientID: '3MVG9uudbyLbNPZMn2emQiwwmoqmcudnURvLui8uICaepT6Egs.LFsHRMAnD00FSog.OXsLKpODzE.jxi.Ffu',
-//         clientSecret: '625133588109438640',
-//         site: 'https://login.salesforce.com',
-//         authorizationPath: '/services/oauth2/authorize',
-//         tokenPath: '/services/oauth2/token',
-//         revokePath: '/services/oauth2/revoke'
-//     },
-// 	token,
-// 	options,
-//     hostURL
 
-// // Initialize the OAuth2 Library
-// var oauth2 = oauth2(credentials)
 
-// // Authorization uri definition
-// var authorization_uri = oauth2.authCode.authorizeURL({
-//         redirect_uri: 'https://hello-world360.herokuapp.com/callback',
-//         scope: 'full'
-//     })
+    // var code = req.query.code
 
-// // Callbacks
-// app.get('/callback', function (req, res) {
-//     var code = req.query.code
+    // oauth2.authCode.getToken({
+    //     code: code,
+    //     grant_type: 'authorization_uri',
+    //     client_id: credentials.clientID,
+    //     client_secret: credentials.clientSecret,
+    //     redirect_uri: 'https://hello-world360.herokuapp.com/callback'
+    // }, saveToken)
 
-//     oauth2.authCode.getToken({
-//         code: code,
-//         grant_type: 'authorization_uri',
-//         client_id: credentials.clientID,
-//         client_secret: credentials.clientSecret,
-//         redirect_uri: 'https://hello-world360.herokuapp.com/callback'
-//     }, saveToken)
-
-//     function saveToken(error, result) {
-//     	console.log('entering saveToken')
-//         if (error) { console.log('Access Token Error', error.message); }
-//         token = oauth2.accessToken.create(result)
+    // function saveToken(error, result) {
+    //     console.log('entering saveToken')
+    //     if (error) { console.log('Access Token Error', error.message); }
+    //     token = oauth2.accessToken.create(result)
         
-//         hostURL = token.token.instance_url.replace('https://', '')
+    //     hostURL = token.token.instance_url.replace('https://', '')
 
-//         options = {
-// 	        host: hostURL,
-// 	        port: 443,
-// 	        path: '/services/data/v35.0/analytics/reports/00Oa0000008r7sg',
-// 	        method: 'GET',
-// 	        headers: {
-// 	            'Authorization': 'Bearer ' + token.token.access_token,
-// 	            'Content-Type': 'application/json'
-// 	        }
-// 	    }
-//     }
+    //     options = {
+    //         host: hostURL,
+    //         port: 443,
+    //         path: '/services/data/v35.0/analytics/reports/00Oa0000008r7sg',
+    //         method: 'GET',
+    //         headers: {
+    //             'Authorization': 'Bearer ' + token.token.access_token,
+    //             'Content-Type': 'application/json'
+    //         }
+    //     }
+    // }
 
-//     res.render('data')
-// })
+    // res.render('data')
+function Authenticate(username, password){
+    var token,
+        tokenConfig = {
+            this.username: username,
+            this.password: password
+        }
 
-// // Initial page redirecting to Salesforce
+    oauth2.password.getToken(tokenConfig, function saveToken(error, result) {
+        if (error) { console.log('Access Token Error', error.message) }
+        token = oauth2.accessToken.create(result)
+
+        oauth2.api('GET', '/users', {
+            access_token: token.token.access_token
+        }, function (err, data) {
+            console.log(data)
+        })
+    })
+}
+
+// Initial page redirecting to Salesforce
 // app.get('/auth', function (req, res) {
 //     res.redirect(authorization_uri)
 // })
-// /************************************************/
+/************************************************/
--- server.js
+++ server.js
@@ -10,28 +10,28 @@ var express    = require('express'),
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-var port = process.env.PORT || 5000;        // set our port
+var port = process.env.PORT || 5000        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router()              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:5000/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+    res.json({ message: 'hooray! welcome to our api!' })
+})
 
 // more routes for our API will happen here
 //route.route('/:id')
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', router)
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Magic happens on port ' + port)
