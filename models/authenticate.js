5aeedebe7b500da8e26bfe7b68cd6165fdf82d85
Sheldon Tan <sheldon.tan@magnet360.com>
2016-06-15 13:54:46 -0500

    updating with class pattern

--- models/authenticate.js
+++ models/authenticate.js
@@ -1,12 +1,7 @@
-// requires
-var https = require('https'),
-    http = require('http'),
-    oauth2 = require('simple-oauth2'),
-    bufferJson = require('buffer-json')
+//Authenticate.js
+//Private
+var oauth2 = require('simple-oauth2')
 
-/* For oauth2 authentication and token handling */
-
-    // Set the configuration settings
 var credentials = {
         clientID: '3MVG9uudbyLbNPZMn2emQiwwmoqmcudnURvLui8uICaepT6Egs.LFsHRMAnD00FSog.OXsLKpODzE.jxi.Ffu',
         clientSecret: '625133588109438640',
@@ -15,73 +10,41 @@ var credentials = {
         tokenPath: '/services/oauth2/token',
         revokePath: '/services/oauth2/revoke'
     },
-    token,
-    options,
-    hostURL
 
 // Initialize the OAuth2 Library
 var oauth2 = oauth2(credentials)
 
-// Authorization uri definition
-var authorization_uri = oauth2.authCode.authorizeURL({
-        redirect_uri: 'https://hello-world360.herokuapp.com/callback',
-        scope: 'full'
-    })
+//Public
+module.exports = Authenticate
 
+function Authenticate(username, password) {
+    this.token = null
+    this.hostURL = null
+    this.tokenconfig.username = username
+    this.tokenconfig.password = password
+}
 
-
-
-    // var code = req.query.code
-
-    // oauth2.authCode.getToken({
-    //     code: code,
-    //     grant_type: 'authorization_uri',
-    //     client_id: credentials.clientID,
-    //     client_secret: credentials.clientSecret,
-    //     redirect_uri: 'https://hello-world360.herokuapp.com/callback'
-    // }, saveToken)
-
-    // function saveToken(error, result) {
-    //     console.log('entering saveToken')
-    //     if (error) { console.log('Access Token Error', error.message); }
-    //     token = oauth2.accessToken.create(result)
+Authenticate.prototype.getToken = function() {
+    oauth2.password.getToken(this.tokenConfig, function saveToken(error, result) {
+    if (error) { 
+        console.log('Access Token Error', error.message) 
+    }
         
-    //     hostURL = token.token.instance_url.replace('https://', '')
+    this.token = oauth2.accessToken.create(result)
+    this.hostURL = this.token.token.instance_url.replace('https://', '')
 
-    //     options = {
-    //         host: hostURL,
-    //         port: 443,
-    //         path: '/services/data/v35.0/analytics/reports/00Oa0000008r7sg',
-    //         method: 'GET',
-    //         headers: {
-    //             'Authorization': 'Bearer ' + token.token.access_token,
-    //             'Content-Type': 'application/json'
-    //         }
-    //     }
-    // }
-
-    // res.render('data')
-function Authenticate(username, password){
-    var token,
-        tokenConfig = {
-            this.username: username,
-            this.password: password
-        }
-
-    oauth2.password.getToken(tokenConfig, function saveToken(error, result) {
-        if (error) { console.log('Access Token Error', error.message) }
-        token = oauth2.accessToken.create(result)
-
-        oauth2.api('GET', '/users', {
+        /*oauth2.api('GET', '/users', {
             access_token: token.token.access_token
         }, function (err, data) {
             console.log(data)
-        })
+        })*/
     })
 }
 
-// Initial page redirecting to Salesforce
-// app.get('/auth', function (req, res) {
-//     res.redirect(authorization_uri)
-// })
-/************************************************/
+
+
+
+
+
+
+
