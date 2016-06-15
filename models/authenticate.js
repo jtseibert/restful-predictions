db5058cb67761cc240cb17e832f23191f8c220d8
Jacob Seibert <12jtseibert@gmail.com>
2016-06-15 16:40:15 -0500

    test

--- models/authenticate.js
+++ models/authenticate.js
@@ -36,7 +36,7 @@ Authenticate.prototype.getToken = function() {
     console.log('tokenConfig: ', tokenConfig.password) 
     oauth2.password.getToken(tokenConfig, function saveToken(error, result) {
     if (error) { 
-        console.log('Access Token Error', error.toString()) 
+        console.log('Access Token Error', error.statusCode()) 
     }
     else {
         this.token = oauth2.accessToken.create(result)
