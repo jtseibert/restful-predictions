c7abccd1019df62ed7e02e6b05e5d8bb538bb073
Jacob Seibert <12jtseibert@gmail.com>
2016-06-16 10:11:42 -0500

    test

--- models/data.js
+++ models/data.js
@@ -16,19 +16,19 @@ var oauth2 = oauth2(credentials)
 function Data(token, id) {
 	this.token = token
 	this.path = token.token.instance_url + 'services/data/v35.0/analytics/reports/' + id
+	this.json
 } 
 
 module.exports = Data
 
 Data.prototype.getData = function(oauth2, callback) {
 	console.log(this.token.token.access_token)
-    callback(
-	    oauth2.api('GET', this.path, {
+	this.json = oauth2.api('GET', this.path, {
 	        'Authorization': 'Bearer ' + this.token.token.access_token,
 	        'Content-Type' : 'application/json'
 	        }, function (err, data) {
 	            console.log(data)
 	    	}
 	    )
-    )
+    callback(this.json)
 }
