329905d21989ae7712e15875a879bba434df7145
Jacob Seibert <12jtseibert@gmail.com>
2016-06-21 16:25:16 -0500

    clearner

--- models/table.js
+++ models/table.js
@@ -15,29 +15,25 @@ Table.prototype.makeTable = function(callback) {
 
 	pg.defaults.ssl = true;
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
-	  if (err) throw err;
-	  console.log('Connected to postgres! Getting schemas...');
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');
 
-	  client
-	    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-	    
-	    client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
-// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
-// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
+		client
+			.query('SELECT table_schema,table_name FROM information_schema.tables;')
 
-var query = client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname");
-query.on("row", function (row, result) {
-    result.addRow(row);
-});
-query.on("end", function (result) {
-    console.log(JSON.stringify(result.rows, null, "    "));
-    client.end();
+		client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
+		// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
+		// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
 
-
-	    .on('row', function(row) {
-	      console.log(JSON.stringify(row));
-	    });
-	});
+		var query = client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname");
+		query.on("row", function (row, result) {
+			result.addRow(row);
+		});
+		query.on("end", function (result) {
+			console.log(JSON.stringify(result.rows, null, "    "));
+			client.end();
+		});
 
 	callback()
+	}
 }
