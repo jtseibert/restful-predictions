f1490f7dbfb5831d2b6e808517511a86120e589e
Jacob Seibert <12jtseibert@gmail.com>
2016-06-21 15:55:28 -0500

    changed req.options

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ router.route('/:instance/:accessToken/:id')
 
 router.route('/table')
 	.post(function(req,res){
-		console.log(req.params)
+		console.log(req.options)
 		// table = new Table(req.params.payload);
 		// table.makeTable()
 		// send table to pg
