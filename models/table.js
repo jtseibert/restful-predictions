137df4e6642034a840c423aa3f93b192895e556b
Sheldon Tan <sheldon.tan@magnet360.com>
2016-06-21 16:22:39 -0500

    DBDBDBD

--- models/table.js
+++ models/table.js
@@ -10,10 +10,34 @@ function Table(csv) {
 } 
 
 Table.prototype.makeTable = function(callback) {
+	//database
+	var pg = require('pg');
+
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+	  if (err) throw err;
+	  console.log('Connected to postgres! Getting schemas...');
+
+	  client
+	    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+	    
+	    client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
+// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
+// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
+
+var query = client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname");
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    client.end();
 
 
-	
-
+	    .on('row', function(row) {
+	      console.log(JSON.stringify(row));
+	    });
+	});
 
 	callback()
 }
--- server.js
+++ server.js
@@ -13,21 +13,6 @@ app.use(bodyParser.urlencoded({limit: '1gb', extended: true }))
 
 var port = process.env.PORT || 5000
 
-//database
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
 //Setup oauth2
 var oauth2 = require('simple-oauth2'),
 	credentials = {
