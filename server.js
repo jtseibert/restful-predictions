6c73c1f95bdce16c2ea82a963fb6841bbf698afd
Jacob Seibert <12jtseibert@gmail.com>
2016-06-21 15:58:15 -0500

    test

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ router.route('/:instance/:accessToken/:id')
 
 router.route('/table')
 	.post(function(req,res){
-		console.log(req.body.payload)
+		console.log(req.body)
 		// table = new Table(req.params.payload);
 		// table.makeTable()
 		// send table to pg
