161039a57ca87826c62dfe380e1a03048d8bd455
Sheldon Tan <sheldon.tan@magnet360.com>
2016-06-21 12:08:28 -0500

    database

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ app.use(bodyParser.json())
 
 var port = process.env.PORT || 5000
 
+//database
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 //Setup oauth2
 var oauth2 = require('simple-oauth2'),
 	credentials = {
