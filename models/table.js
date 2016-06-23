2b8922bbad971b21a4791d8290a1bbd4aa0430f6
Jacob Seibert <12jtseibert@gmail.com>
2016-06-23 12:22:20 -0500

    test

--- models/table.js
+++ models/table.js
@@ -12,7 +12,7 @@ function Table(data) {
 Table.prototype.saveTable = function(client, callback) {
 
 	//client.query('INSERT INTO allocation_reports(id, json) values($1, $2)', [this.id, this.json])
-	client.query('WITH UPSERT AS (UPDATE allocation_reports SET (id, json) = ($1, $2) WHERE id = $1', [this.id, this.json])
+	client.query('WITH UPSERT AS (UPDATE allocation_reports SET (id, json) = ($1, $2) WHERE id = $1)', [this.id, this.json])
 	// client.query("IF EXISTS (SELECT id FROM allocation_reports WHERE id=$1) THEN "
 	// 				+ "UPDATE allocation_reports SET id=$1 json=$2 ELSE "
 	// 				+ "INSERT INTO allocation_reports(id, json) values($1, $2)",
