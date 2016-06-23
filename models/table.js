a6ec9d9bcf2540dc86d8ae89e56d01044c2f97f1
Jacob Seibert <12jtseibert@gmail.com>
2016-06-23 11:44:25 -0500

    test

--- models/table.js
+++ models/table.js
@@ -4,37 +4,17 @@
 	
 module.exports = Table
 
-function Table(data, pg) {
+function Table(data) {
 	this.json 		= data.json
 	this.id 		= data.id
-	this.dataBase 	= pg
 } 
 
-Table.prototype.saveTable = function(callback) {
-	var id = this.id
-	var json = this.json
-	this.dataBase.connect(process.env.DATABASE_URL, function(err, client, id, json) {
-		if (err) throw err;
-		//console.log('Connected to postgres! Getting schemas...');
-		//client.query('SELECT table_schema,table_name FROM information_schema.tables;')
-		//client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
-		// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
-		// client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
-		// var query = client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname");
-		// query.on("row", function (row, result) {
-		// 	result.addRow(row);
-		// });
-		// query.on("end", function (result) {
-		// 	console.log(JSON.stringify(result.rows, null, "    "));
-		// 	client.end();
-		// });
+Table.prototype.saveTable = function(client, callback) {
 
-		console.log(id)
-		console.log(json[2506])
+	//console.log(this.id)
+	//console.log(this.json[2506])
 
-
-		client.query("INSERT INTO allocation_reports(id, json) values($1, $2)", [this.id, this.json]);
-	});
+	client.query('INSERT INTO allocations_reports(id, json) values($1, $2)', [this.id, this.json])
 
 	callback()
 
--- server.js
+++ server.js
@@ -46,12 +46,30 @@ router.route('/:instance/:accessToken/:id')
 router.route('/table')
 	.post(function(req,res){
 		//console.log(req.body)
-		table = new Table(req.body, pg);
-		table.saveTable(function(err){
-			if (err)
-				res.send(err)
-			res.json({message: 'Success!'})
-		})
+		table = new Table(req.body);
+
+
+		pg.connect(process.env.DATABASE_URL, function(err, client) {
+			if (err) throw err;
+			// console.log('Connected to postgres! Getting schemas...');
+			// client.query('SELECT table_schema,table_name FROM information_schema.tables;')
+			// client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
+			// // client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
+			// // client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
+			// var query = client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname");
+			// query.on("row", function (row, result) {
+			// 	result.addRow(row);
+			// });
+			// query.on("end", function (result) {
+			// 	console.log(JSON.stringify(result.rows, null, "    "));
+			// 	client.end();
+			// });
+			table.saveTable(client,function(err){
+				if (err)
+					res.send(err)
+				res.json({message: 'Success!'})
+			})
+		}
 		// send table to pg
 	})
 
