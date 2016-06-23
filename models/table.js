acb811a90ebf4462293b31888c053e6f67da3964
Jacob Seibert <12jtseibert@gmail.com>
2016-06-23 14:23:04 -0500

    test

--- models/table.js
+++ models/table.js
@@ -11,30 +11,19 @@ function Table(data) {
 
 Table.prototype.saveTable = function(client, callback) {
 
-	//client.query('INSERT INTO allocation_reports(id, json) values($1, $2)', [this.id, this.json])
+	client.query('INSERT INTO allocation_reports(id, json) values($1, $2) ON CONFLICT (id) DO UPDATE SET json = $2', [this.id, this.json])
+
+
+
+
+
+
+
 	//client.query('WITH UPSERT AS (UPDATE allocation_reports SET (id, json) = ($1, $2) WHERE id = $1 return )', [this.id, this.json])
 	// client.query("IF EXISTS (SELECT id FROM allocation_reports WHERE id=$1) THEN "
 	// 				+ "UPDATE allocation_reports SET id=$1 json=$2 ELSE "
 	// 				+ "INSERT INTO allocation_reports(id, json) values($1, $2)",
 	// 				[this.id, this.json])
-
-
-	// client.query({
- //  		text: "SELECT upsert($1, $2)",
- //  		values: [ this.id, 
- //            		this.json
- //            	]
-	// }, function(err, result){
- //  		if(err) 
- //  			console.log(err)
-	// 	})
-
-	client.query(
-		"if exist(select * from allocation_reports where id=$1) {"
-	    + "update allocation_reports set (json = $2) where id=$1"
-		+ "} else{"
-	    + "insert into allocation_reports values ($1, $2)}", [this.id, this.json]
-	)
 
 	//testing
 	var query = client.query("SELECT json -> '2506' ->> 'Employees' AS shouldbe1person FROM allocation_reports");
