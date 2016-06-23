d4faf38873da8267f579d5df35f1da1652cc535e
Jacob Seibert <12jtseibert@gmail.com>
2016-06-23 12:31:42 -0500

    test

--- models/table.js
+++ models/table.js
@@ -12,11 +12,24 @@ function Table(data) {
 Table.prototype.saveTable = function(client, callback) {
 
 	//client.query('INSERT INTO allocation_reports(id, json) values($1, $2)', [this.id, this.json])
-	client.query('WITH UPSERT AS (UPDATE allocation_reports SET (id, json) = ($1, $2) WHERE id = $1)', [this.id, this.json])
+	//client.query('WITH UPSERT AS (UPDATE allocation_reports SET (id, json) = ($1, $2) WHERE id = $1 return )', [this.id, this.json])
 	// client.query("IF EXISTS (SELECT id FROM allocation_reports WHERE id=$1) THEN "
 	// 				+ "UPDATE allocation_reports SET id=$1 json=$2 ELSE "
 	// 				+ "INSERT INTO allocation_reports(id, json) values($1, $2)",
 	// 				[this.id, this.json])
+
+
+	client.query({
+  		text: "SELECT upsert($1, $2)",
+  		values: [ this.id, 
+            		this.json
+            	]
+	}, function(u_err, u_result){
+  		if(err) // this is a real error, handle it
+  			console.log(err)
+  		// otherwise your data is updated or inserted properly
+		}});
+
 
 	//testing
 	var query = client.query("SELECT * FROM allocation_reports");
