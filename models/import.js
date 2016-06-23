20b525520ea922a8babb03508cb01050b59a5f35
Jacob Seibert <12jtseibert@gmail.com>
2016-06-23 14:58:22 -0500

    finalized import

--- models/import.js
+++ models/import.js
@@ -14,12 +14,10 @@ Import.prototype.getJsonData = function(client, callback) {
 	var results = []
 
 	var query = client.query('SELECT json FROM allocation_reports WHERE id = $1', [this.sheetName])
-
         // Stream results back one row at a time
         query.on('row', function(row) {
             results.push(row)
         })
-
         // After all data is returned, close connection and return results
         query.on('end', function() {
             callback(results)
