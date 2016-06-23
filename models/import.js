fdd9ede5524a9f3913863e4870fb0e64959c230d
Jacob Seibert <12jtseibert@gmail.com>
2016-06-23 14:53:01 -0500

    test

--- models/import.js
+++ models/import.js
@@ -10,6 +10,19 @@ function Import(sheetName) {
 } 
 
 Import.prototype.getJsonData = function(client, callback) {
+
+	var results = []
+
 	var query = client.query('SELECT json FROM allocation_reports WHERE id = $1' [this.sheetName])
-	callback(query)
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row)
+        })
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done()
+            callback(results)
+        })
 }
--- models/table.js
+++ models/table.js
@@ -13,18 +13,6 @@ Table.prototype.saveTable = function(client, callback) {
 
 	client.query('INSERT INTO allocation_reports(id, json) values($1, $2) ON CONFLICT (id) DO UPDATE SET json = $2', [this.id, this.json])
 
-
-
-
-
-
-
-	//client.query('WITH UPSERT AS (UPDATE allocation_reports SET (id, json) = ($1, $2) WHERE id = $1 return )', [this.id, this.json])
-	// client.query("IF EXISTS (SELECT id FROM allocation_reports WHERE id=$1) THEN "
-	// 				+ "UPDATE allocation_reports SET id=$1 json=$2 ELSE "
-	// 				+ "INSERT INTO allocation_reports(id, json) values($1, $2)",
-	// 				[this.id, this.json])
-
 	//testing
 	var query = client.query("SELECT json -> '2506' ->> 'Employees' AS shouldbe1person FROM allocation_reports");
 	query.on("row", function (row, result) {
