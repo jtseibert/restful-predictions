1b14deb0ae4eca397a3ec56ad867a360a02e2707
Jacob Seibert <12jtseibert@gmail.com>
2016-06-23 12:05:30 -0500

    test

--- models/table.js
+++ models/table.js
@@ -15,6 +15,14 @@ Table.prototype.saveTable = function(client, callback) {
 	//console.log(this.json[2506])
 
 	client.query('INSERT INTO allocation_reports(id, json) values($1, $2)', [this.id, this.json])
+	var query = client.query("SELECT * FROM allocation_reports");
+	query.on("row", function (row, result) {
+		result.addRow(row);
+	});
+	query.on("end", function (result) {
+		console.log(JSON.stringify(result.rows, null, "    "));
+		client.end();
+	});
 
 	callback()
 
