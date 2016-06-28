1284f080f3c1d8a3466c437a5081d47092ae59c6
Jacob Seibert <12jtseibert@gmail.com>
2016-06-28 13:51:52 -0500

    typo

--- models/updateDB.js
+++ models/updateDB.js
@@ -12,11 +12,11 @@ function UpdateDB(data) {
 
 UpdateDB.prototype.updateDB = function(client, callback) {
 
-	client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunitiy) DO UPDATE SET probability = $2, stage = $3', 
+	client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunity) DO UPDATE SET probability = $2, stage = $3', 
 					[this.opportunitiy, this.stage, this.probability])
 
 	//testing
-	var query = client.query("SELECT * from opportunitiy_pipeline");
+	var query = client.query("SELECT * from opportunity_pipeline");
 	query.on("row", function (row, result) {
 		result.addRow(row);
 	});
