7042b9c9214caa53bf121f0659feba0ad3e51497
Jacob Seibert <12jtseibert@gmail.com>
2016-06-28 14:53:32 -0500

    test

--- models/pipeline.js
+++ models/pipeline.js
@@ -10,61 +10,68 @@ Pipeline.prototype.getPipeline = function(client, oauth2, callback) {
 	parameters = {
 		access_token: this.accessToken
 	}
-	var dbData,
+	var dbData = {},
 		query = client.query("SELECT * from opportunity_pipeline");
 	query.on("row", function (row, result) {
 		result.addRow(row)
 	});
 	query.on("end", function (result) {
-		dbData = result
-		console.log(JSON.stringify(dbData.rows));
+		
+	for (var entry in result.rows){
+		dbData[result.rows[row].opportunity] = {
+			"STAGE": result.rows[row].stage,
+			"PROBABILITY": result.rows[row].probability
+		}
+	}
+		console.log(JSON.stringify(dbData))
 		client.end()
 	});
 
-	oauth2.api('GET', this.path, parameters, function (err, data) {
-	    if (err)
-	        console.log('GET Error: ', JSON.stringify(err)) 
+
+	// oauth2.api('GET', this.path, parameters, function (err, data) {
+	//     if (err)
+	//         console.log('GET Error: ', JSON.stringify(err)) 
 	    
-	    var factMap 				= data.factMap,
-	    	groupingsDown 			= data.groupingsDown.groupings,
-	    	returnData				= [],
-	    	rowData,
-	    	stageKey
+	//     var factMap 				= data.factMap,
+	//     	groupingsDown 			= data.groupingsDown.groupings,
+	//     	returnData				= [],
+	//     	rowData,
+	//     	stageKey
 
-	    returnData.push(["STAGE",
-	    					"OPPORTUNITY_NAME",
-      						"TYPE",
-							"LEAD_SOURCE",
-							"AMOUNT",
-							"EXP_AMOUNT",
-							"CLOSE_DATE",
-							"NEXT_STEP",
-							"PROBABILITY",
-							"FISCAL_QUARTER",
-							"AGE",
-							"CREATED_DATE",
-							"FULL_NAME",
-							"ROLLUP_DESCRIPTION",
-							"ACCOUNT_NAME"
-						])
+	//     returnData.push(["STAGE",
+	//     					"OPPORTUNITY_NAME",
+ //      						"TYPE",
+	// 						"LEAD_SOURCE",
+	// 						"AMOUNT",
+	// 						"EXP_AMOUNT",
+	// 						"CLOSE_DATE",
+	// 						"NEXT_STEP",
+	// 						"PROBABILITY",
+	// 						"FISCAL_QUARTER",
+	// 						"AGE",
+	// 						"CREATED_DATE",
+	// 						"FULL_NAME",
+	// 						"ROLLUP_DESCRIPTION",
+	// 						"ACCOUNT_NAME"
+	// 					])
 
-	    for (var stage in factMap) {
+	//     for (var stage in factMap) {
 
-		    stageKey = stage.split('!')[0];
+	// 	    stageKey = stage.split('!')[0];
 			
 
-			if (stageKey != "T"){
-				for (var row in factMap[stage].rows){
-					rowData = []
-					rowData.push(groupingsDown[stageKey].label)
-					for (var cell in factMap[stage].rows[row].dataCells){
-						rowData.push(factMap[stage].rows[row].dataCells[cell].label)
-					}
-					returnData.push(rowData)
-				}
-			}
-		}
-		console.log(returnData)
-	    callback(returnData)
-	})  
+	// 		if (stageKey != "T"){
+	// 			for (var row in factMap[stage].rows){
+	// 				rowData = []
+	// 				rowData.push(groupingsDown[stageKey].label)
+	// 				for (var cell in factMap[stage].rows[row].dataCells){
+	// 					rowData.push(factMap[stage].rows[row].dataCells[cell].label)
+	// 				}
+	// 				returnData.push(rowData)
+	// 			}
+	// 		}
+	// 	}
+	// 	console.log(returnData)
+	//     callback(returnData)
+	// })  
 }
