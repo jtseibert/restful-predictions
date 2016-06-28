f106a58324bd16d67e88bccb105cd8fb5d342b68
Jacob Seibert <12jtseibert@gmail.com>
2016-06-28 13:58:26 -0500

    added in test sttents ;

--- models/updateDB.js
+++ models/updateDB.js
@@ -12,18 +12,22 @@ function UpdateDB(data) {
 
 UpdateDB.prototype.updateDB = function(client, callback) {
 
-	client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunity) DO UPDATE SET probability = $2, stage = $3', 
-					[this.opportunitiy, this.stage, this.probability])
+	console.log(this.stage)
+	console.log(this.opportunity)
+	console.log(this.probability)
 
-	//testing
-	var query = client.query("SELECT * from opportunity_pipeline");
-	query.on("row", function (row, result) {
-		result.addRow(row);
-	});
-	query.on("end", function (result) {
-		console.log(JSON.stringify(result.rows, null, "    "));
-		client.end();
-	});
+	// client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunity) DO UPDATE SET probability = $2, stage = $3', 
+	// 				[this.opportunitiy, this.stage, this.probability])
+
+	// //testing
+	// var query = client.query("SELECT * from opportunity_pipeline");
+	// query.on("row", function (row, result) {
+	// 	result.addRow(row);
+	// });
+	// query.on("end", function (result) {
+	// 	console.log(JSON.stringify(result.rows, null, "    "));
+	// 	client.end();
+	// });
 
 	callback()
 }
