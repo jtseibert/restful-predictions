f259bf927b6a5469f8362cba38314d01cda909a1
Jacob Seibert <12jtseibert@gmail.com>
2016-06-28 14:02:01 -0500

    typos done

--- models/updateDB.js
+++ models/updateDB.js
@@ -12,22 +12,18 @@ function UpdateDB(data) {
 
 UpdateDB.prototype.updateDB = function(client, callback) {
 
-	console.log(this.stage)
-	console.log(this.opportunity)
-	console.log(this.probability)
+	client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunity) DO UPDATE SET probability = $2, stage = $3', 
+					[this.opportunitiy, this.stage, this.probability])
 
-	// client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunity) DO UPDATE SET probability = $2, stage = $3', 
-	// 				[this.opportunitiy, this.stage, this.probability])
-
-	// //testing
-	// var query = client.query("SELECT * from opportunity_pipeline");
-	// query.on("row", function (row, result) {
-	// 	result.addRow(row);
-	// });
-	// query.on("end", function (result) {
-	// 	console.log(JSON.stringify(result.rows, null, "    "));
-	// 	client.end();
-	// });
+	//testing
+	var query = client.query("SELECT * from opportunity_pipeline");
+	query.on("row", function (row, result) {
+		result.addRow(row);
+	});
+	query.on("end", function (result) {
+		console.log(JSON.stringify(result.rows, null, "    "));
+		client.end();
+	});
 
 	callback()
 }
