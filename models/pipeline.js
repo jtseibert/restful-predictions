b714d1521f86394725fcfc3e515be0cfea375791
Jacob Seibert <12jtseibert@gmail.com>
2016-06-28 12:06:51 -0500

    changed report ID

--- models/pipeline.js
+++ models/pipeline.js
@@ -2,7 +2,7 @@ module.exports = Pipeline
 
 function Pipeline(instance, accessToken) {
 	this.accessToken = accessToken
-	this.path = 'https://' + instance + '/services/data/v35.0/analytics/reports/00Oa00000093sBK'
+	this.path = 'https://' + instance + '/services/data/v35.0/analytics/reports/00Oa00000093sCD'
 } 
 
 Pipeline.prototype.getPipeline = function(oauth2, callback) {
