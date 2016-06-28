52acf0c38da354ab6a2403025edbd82f8812e48f
Jacob Seibert <12jtseibert@gmail.com>
2016-06-28 15:33:12 -0500

    testing overriding pull from SF

--- models/pipeline.js
+++ models/pipeline.js
@@ -23,56 +23,62 @@ Pipeline.prototype.getPipeline = function(client, oauth2, callback) {
 			"PROBABILITY": result.rows[entry].probability
 		}
 	}
-		if (dbData['Black Hills / SourceGas-CRM Implementation'])
-			console.log(JSON.stringify(dbData))
 		client.end()
 	});
 
 
-	// oauth2.api('GET', this.path, parameters, function (err, data) {
-	//     if (err)
-	//         console.log('GET Error: ', JSON.stringify(err)) 
+	oauth2.api('GET', this.path, parameters, function (err, data) {
+	    if (err)
+	        console.log('GET Error: ', JSON.stringify(err)) 
 	    
-	//     var factMap 				= data.factMap,
-	//     	groupingsDown 			= data.groupingsDown.groupings,
-	//     	returnData				= [],
-	//     	rowData,
-	//     	stageKey
+	    var factMap 				= data.factMap,
+	    	groupingsDown 			= data.groupingsDown.groupings,
+	    	returnData				= [],
+	    	opportunityIndex		= 1,
+	    	stageIndex				= 0,
+	    	probabilityIndex		= 8,
+	    	rowData,
+	    	stageKey
 
-	//     returnData.push(["STAGE",
-	//     					"OPPORTUNITY_NAME",
- //      						"TYPE",
-	// 						"LEAD_SOURCE",
-	// 						"AMOUNT",
-	// 						"EXP_AMOUNT",
-	// 						"CLOSE_DATE",
-	// 						"NEXT_STEP",
-	// 						"PROBABILITY",
-	// 						"FISCAL_QUARTER",
-	// 						"AGE",
-	// 						"CREATED_DATE",
-	// 						"FULL_NAME",
-	// 						"ROLLUP_DESCRIPTION",
-	// 						"ACCOUNT_NAME"
-	// 					])
+	    returnData.push(["STAGE",
+	    					"OPPORTUNITY_NAME",
+      						"TYPE",
+							"LEAD_SOURCE",
+							"AMOUNT",
+							"EXP_AMOUNT",
+							"CLOSE_DATE",
+							"NEXT_STEP",
+							"PROBABILITY",
+							"FISCAL_QUARTER",
+							"AGE",
+							"CREATED_DATE",
+							"FULL_NAME",
+							"ROLLUP_DESCRIPTION",
+							"ACCOUNT_NAME"
+						])
 
-	//     for (var stage in factMap) {
+	    for (var stage in factMap) {
 
-	// 	    stageKey = stage.split('!')[0];
+		    stageKey = stage.split('!')[0];
 			
 
-	// 		if (stageKey != "T"){
-	// 			for (var row in factMap[stage].rows){
-	// 				rowData = []
-	// 				rowData.push(groupingsDown[stageKey].label)
-	// 				for (var cell in factMap[stage].rows[row].dataCells){
-	// 					rowData.push(factMap[stage].rows[row].dataCells[cell].label)
-	// 				}
-	// 				returnData.push(rowData)
-	// 			}
-	// 		}
-	// 	}
-	// 	console.log(returnData)
-	//     callback(returnData)
-	// })  
+			if (stageKey != "T"){
+				for (var row in factMap[stage].rows){
+					rowData = []
+					rowData.push(groupingsDown[stageKey].label)
+					for (var cell in factMap[stage].rows[row].dataCells){
+						rowData.push(factMap[stage].rows[row].dataCells[cell].label)
+					}
+					if(dbData[factMap[stage].rows[row].dataCells[opportunityIndex].label]){
+						rowData[stageIndex] = factMap[stage].rows[row].dataCells[stageIndex].label
+						rowData[opportunityIndex] = factMap[stage].rows[row].dataCells[opportunityIndex].label
+						rowData[probabilityIndex] = factMap[stage].rows[row].dataCells[probabilityIndex].label
+					}
+					returnData.push(rowData)
+				}
+			}
+		}
+		console.log(returnData)
+	    callback(returnData)
+	})  
 }
