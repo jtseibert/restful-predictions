9e981865c11f74167e17b58749adeec3bd49bab6
Jacob Seibert <12jtseibert@gmail.com>
2016-06-29 12:57:00 -0500

    test

--- models/updateDB.js
+++ models/updateDB.js
@@ -10,6 +10,8 @@ function UpdateDB(data) {
 
 UpdateDB.prototype.updateDB = function(client, callback) {
 
+	console.log(this.data)
+
 	for (var entry in this.data){
 		client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunity) DO UPDATE SET stage = $2, probability = $3', 
 						[this.data[entry].opportunity, this.data[entry].stage, this.data[entry].probability])
