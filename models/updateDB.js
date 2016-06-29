4da343d1cbdaf1239365ae11aee723e7778a5a34
Jacob Seibert <12jtseibert@gmail.com>
2016-06-29 11:34:00 -0500

    can update multiple updates

--- models/updateDB.js
+++ models/updateDB.js
@@ -16,16 +16,16 @@ UpdateDB.prototype.updateDB = function(client, callback) {
 	for (var entry in this.data){
 		client.query('INSERT INTO opportunity_pipeline(opportunity, stage, probability) values($1, $2, $3) ON CONFLICT (opportunity) DO UPDATE SET stage = $2, probability = $3', 
 						[this.data[entry].opportunity, this.data[entry].stage, this.data[entry].probability])
-
-		//testing
-		var query = client.query("SELECT * from opportunity_pipeline")
-		query.on("row", function (row, result) {
-			result.addRow(row)
-		})
-		query.on("end", function (result) {
-			console.log(JSON.stringify(result.rows, null, "    "))
-		})
 	}
-	client.end()
+
+	//testing
+	var query = client.query("SELECT * from opportunity_pipeline")
+	query.on("row", function (row, result) {
+		result.addRow(row)
+	})
+	query.on("end", function (result) {
+		console.log(JSON.stringify(result.rows, null, "    "))
+		client.end()
+	})
 	callback()
 }
