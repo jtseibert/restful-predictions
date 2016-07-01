957c2d5e9a290e8359bc18d78e60a02885fb3dd5
Jacob Seibert <12jtseibert@gmail.com>
2016-07-01 09:50:49 -0500

    test

--- models/pipeline.js
+++ models/pipeline.js
@@ -137,6 +137,7 @@ Pipeline.prototype.getPipeline = function(client, oauth2, callback) {
 function calculateStartDate(closeDate){
 	var date = new Date(closeDate)
 	var returnDate = new Date(date.setDate(date.getDate() + 7))
-	returnDate = returnDate.split('T')[0].replace('-','/')
+	returnDate = returnDate.split('T')[0].split('-')
+	returnDate = returnDate[1]+'/'+returnDate[2]+'/'+returnDate[0]
 	return returnDate
 }
