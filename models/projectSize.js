8c2f90e94395b3dc5d419d5b5d8f26c0431e0c9b
Jacob Seibert <12jtseibert@gmail.com>
2016-07-07 12:05:45 -0500

    test

--- models/projectSize.js
+++ models/projectSize.js
@@ -1,23 +1,23 @@
 //projectSize.js
 //input: 
-	//JSON of a project size to add to project_sizes or remove from project_sizes
+	//JSON of a project size to add to project_size or remove from project_size
 	
-module.exports = projectSize
+module.exports = ProjectSize
 
 function ProjectSize(data) {
 	this.data = data
 } 
 
 ProjectSize.prototype.add = function(client, callback) {
-
 	console.log(this.data)
 
 	for (var entry in this.data){
-		client.query('INSERT INTO omit(opportunity) values($1)', [entry])
+		client.query('INSERT INTO project_size(sizeId,pricehigh,roles_allocations,numweeks) values($1,$2,$3,$4)',
+			[entry,this.data[entry].pricehigh,this.data[entry].roles_allocations,this.data[entry].numweeks])
 	}
 
 	//testing
-	var query = client.query("SELECT * from omit")
+	var query = client.query("SELECT * from project_size")
 	query.on("row", function (row, result) {
 		result.addRow(row)
 	})
@@ -28,15 +28,24 @@ ProjectSize.prototype.add = function(client, callback) {
 	callback()
 }
 
-Omit.prototype.remove = function(client, callback) {
+ProjectSize.prototype.update = function(client, callback) {
 
+
+}
+
+ProjectSize.prototype.getProjectSize = function(client, callback) {
+
+
+}
+
+ProjectSize.prototype.remove = function(client, callback) {
 	for (var entry in this.data){
 		console.log('should be deleting: ' + entry)
-		client.query('DELETE FROM omit WHERE opportunity = $1', [entry])
+		client.query('DELETE FROM project_size WHERE opportunity = $1', [entry])
 	}
 
 	//testing
-	var query = client.query("SELECT * from omit")
+	var query = client.query("SELECT * from project_size")
 	query.on("row", function (row, result) {
 		result.addRow(row)
 	})
@@ -45,21 +54,8 @@ Omit.prototype.remove = function(client, callback) {
 		client.end()
 	})
 	callback()
+
 }
-
-Omit.prototype.getOmit = function(client, callback) {
-	var query = client.query('SELECT * FROM omit')
-	query.on("row", function (row, result) {
-		result.addRow(row)
-	})
-	query.on("end", function (result) {
-		console.log(JSON.stringify(result.rows, null, "    "))
-		client.end()
-		callback(result.rows)
-	})
-}
-
-
 
 
 
