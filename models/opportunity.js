0fc0abe5e94c3449e2bd942ddcbc239b6393bbe2
Jacob Seibert <12jtseibert@gmail.com>
2016-07-11 11:47:26 -0500

    test

--- models/opportunity.js
+++ models/opportunity.js
@@ -12,7 +12,7 @@ Opportunity.prototype.add = function(client, callback) {
 
 	for (var entry in this.data){
 
-		this.data[entry].expected_amount = this.data[entry].expected_amount.replace('USD ', '').replace(/,/g,'')
+		this.data[entry].expected_amount = this.data[entry].expected_amount.replace('USD ', '').replace(/,/g,'').split('.')[0]
 
 		console.log(this.data)
 
