293adf831f292d20605aa5fc71e6b9f44ade3804
Jacob Seibert <12jtseibert@gmail.com>
2016-07-14 08:57:21 -0500

    testing getMonday

--- models/pipeline.js
+++ models/pipeline.js
@@ -196,7 +196,7 @@ Pipeline.prototype.get = function(client, oauth2, callback) {
 
 function calculateStartDate(closeDate, dateIncrement){
 	var date = new Date(closeDate)
-	var returnDate = new Date(date.setDate(date.getDate() + dateIncrement))
+	var returnDate = getMonday(new Date(date.setDate(date.getDate() + dateIncrement)))
 	returnDate = JSON.stringify(returnDate).split('T')[0].split('-')
 	return returnDate[1]+'/'+returnDate[2]+'/'+returnDate[0].replace('"','')
 }
@@ -241,5 +241,10 @@ function getProjectSize(expectedAmount){
 
 }
 
-
+function getMonday(d) {
+	d = new Date(d)
+	var day = d.getDay(),
+		diff = d.getDate() - day + (day == 0 ? -6:1)
+	return new Date(d.setDate(diff))
+}
 
