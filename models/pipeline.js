66f651723be186be841fe5ac3476d5561183ba84
Sheldon Tan <sheldon.tan@magnet360.com>
2016-07-15 15:26:39 -0500

    passing in oath

--- models/pipeline.js
+++ models/pipeline.js
@@ -13,7 +13,7 @@ Pipeline.prototype.get = function(client, oauth2, cache, callback) {
 
 	console.log(this.accessToken)
 	var pipelineCache = new cache();
-    pipelineCache.get("sales_pipeline", function(err, value) {
+    pipelineCache.get("sales_pipeline", function(err, oath2, value) {
     	if(value == undefined) {
     		//Do what we had originaly and store in cache
     		projectSizes = {}
