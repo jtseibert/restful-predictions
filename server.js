5c582f5473f69a60b93940d2dda7ea884e2896ba
Jacob Seibert <12jtseibert@gmail.com>
2016-07-18 12:32:08 -0500

    test

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ router.route('/:instance/DATA_Sales_Pipeline/:accessToken')
 			pg.connect(process.env.DATABASE_URL, function(err, client) {
 				if (err) throw err
 				pipeline.get(client, oauth2, async, cache, function(result) {
+					console.log(result)
 					res.json(result)
 					client.end()
 					delete pipeline
