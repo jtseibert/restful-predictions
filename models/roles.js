fbf366cba5f1dde10387c2646f0ac7659eb74883
Sheldon Tan <sheldon.tan@magnet360.com>
2016-07-20 13:35:59 -0500

    log

--- models/roles.js
+++ models/roles.js
@@ -20,7 +20,7 @@ Roles.prototype.get = function(pg, callback) {
 }
 
 Roles.prototype.add = function(pg, callback) {
-	var role = this.data.role
+	var role = this.data
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
 		console.log(role)
 		//var query = client.query('INSERT INTO roles (role) values ($1) ON CONFLICT (role) DO NOTHING',
