958ab8b03d309429bc45894c704c714fe94085e0
Jacob Seibert <12jtseibert@gmail.com>
2016-07-22 15:34:30 -0500

    removed some console.logs from capacity

--- models/capacity.js
+++ models/capacity.js
@@ -45,8 +45,6 @@ Capacity.prototype.get = function(callback) {
 	    	headers.push(header.label)
 	    	process.nextTick(callback)
 	    }, function(err){
-	    	if (err)
-	    		console.log(err)
 			objInstance.returnData.push(headers)
 			async.each(rows, function(row, callback){
 				var tempRow = []
@@ -65,12 +63,10 @@ Capacity.prototype.get = function(callback) {
 }
 
 Capacity.prototype.updateDB = function(callback){
-	console.log('updateDB')
 	objInstance = this
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		async.eachOf(objInstance.returnData, function(row, rowNumber, callback){
 			if (rowNumber != 0) {
-				console.log('row: '+row[1])
 				client.query('INSERT INTO capacity(contact_id, name, title, available_hours) VALUES($3,$1,'
 								+'(SELECT role FROM roles WHERE role=$2),$4) ON CONFLICT (contact_id) DO UPDATE SET title=(SELECT role FROM roles WHERE role=$2)',
 								[row[0],row[1],row[2],40])
