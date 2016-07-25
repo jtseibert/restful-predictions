a48ea97bca1c91b46b543b6a6fe019ac71d16496
Sheldon Tan <sheldon.tan@magnet360.com>
2016-07-25 12:27:05 -0500

    server chang

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ router.route('/:instance/DATA_Allocation/:accessToken')
 			if(!err) {
 				if(value == undefined) {
 		    		console.log('allocation data not cached')
-					allocation.getReport(oauth2, async, cache, function() {
-						res.json(allocation.returnData)
+					allocation.getReport(oauth2, async, cache, function(data) {
+						res.json(data)
 						delete allocation
 					})
 				} else { 
