33be4edf41372f9a994dbc66d01fbe69ff2b99ea
Sheldon Tan <sheldon.tan@magnet360.com>
2016-07-25 09:49:58 -0500

    broken inpt

--- models/allocation2.js
+++ models/allocation2.js
@@ -25,7 +25,8 @@ Allocation2.prototype.getReport = function(oauth2, async, cache, callback) {
 				roleList[currentRole.key] = currentRole.label
 			}
 			//mapValues getRoleData
-			async.mapValues(roleList, getRoleData(role, roleKey))
+			console.log(roleList)
+			//async.mapValues(roleList, getRoleData(role, roleKey))
 
 		}
 	})
