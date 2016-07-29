4662ec3e5e0fb7a0b2578fa180a18a0faac62f98
Sheldon Tan <sheldon.tan@magnet360.com>
2016-07-29 10:18:32 -0500

    log

--- server.js
+++ server.js
@@ -288,6 +288,7 @@ router.route('/importProjectSize')
 	.post(function(req, res){
 		//console.log(req.body)
 		for(var b in req.body) {
+			console.log(b)
 			var workbook = xls.read(b, {type:"binary"})
 			var json = xls.Utils.sheet_to_json(workbook)
 			console.log(json)
