9ff90634d4818d0923bc5c985b8a9551e59cc82d
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-02 13:04:32 -0500

    cell numeral

--- server.js
+++ server.js
@@ -295,7 +295,7 @@ router.route('/importProjectSize')
     		//console.log(z + "=" + JSON.stringify(sheet[z].v));
   		}
 		//var json = xlsx.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[2]])
-		console.log(sheet['B19'].v)
+		console.log(sheet[{c:1,r:19}].v)
 		res.send({message: "Success!"})
 	})
 
