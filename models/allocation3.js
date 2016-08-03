76d6fadd6954f43bc326125522fe246d551d407a
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-03 12:14:52 -0500

    changed path

--- models/allocation3.js
+++ models/allocation3.js
@@ -27,7 +27,7 @@ roles to the getRoleData method. getRoleData is executed asyncronously on every 
 */
 Allocation3.prototype.getReportData = function(accessToken, path, callback) {
 	var conn = new sf.Connection({
-	  instanceUrl: path,
+	  instanceUrl: 'https://' + path,
 	  accessToken: accessToken
 	})
 
