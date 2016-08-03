402eb68eff12decdcfaac39a12f2fa3826972a75
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-03 15:01:43 -0500

    test

--- models/allocation3.js
+++ models/allocation3.js
@@ -33,8 +33,9 @@ Allocation3.prototype.querySF = function(accessToken, path, callback) {
 	conn.query("SELECT pse__Resource__r.ContactID_18__c, pse__Resource__r.Name, pse__Project__r.Name, pse__Resource__r.pse__Resource_Role__c, pse__Estimated_Hours__c, pse__Start_Date__c FROM pse__Est_Vs_Actuals__c WHERE pse__Estimated_Hours__c>0 AND pse__Resource__r.pse__Exclude_from_Resource_Planner__c=False AND pse__End_Date__c>=2016-08-03 AND pse__End_Date__c<2017-02-03 AND pse__Resource__r.ContactID_18__c!=null")
   	.on("record", function(record) {
   		var recordData = []
+  		console.log(record)
   		// Push all fields of a single record
-    	recordData.push(
+    	/*recordData.push(
     		record[pse__Resource__r].ContactID_18__c,
 			record[pse__Resource__r].Name,
 			record[pse__Resource__r].pse__Resource_Role__c,
@@ -43,7 +44,7 @@ Allocation3.prototype.querySF = function(accessToken, path, callback) {
 			record.pse__Start_Date__c
 		);
 		console.log(recordData)
-    	allocationData.push(recordData)
+    	allocationData.push(recordData)*/
 		})
 	.on("end", function(query) {
 		console.log("total in database : " + query.totalSize);
