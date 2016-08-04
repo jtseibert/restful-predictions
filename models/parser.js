fa954146f5bd3a4af4ce5523db1e2c3b89078fbf
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-04 11:32:29 -0500

    column lmit

--- models/parser.js
+++ models/parser.js
@@ -8,12 +8,15 @@ var xlsx = require('xlsx')
 var parseExcelSheet = function(b64String) {
 	var workbook = xlsx.read(b64String, {type: 'base64'})
 	var sheet 	 = workbook.Sheets[workbook.SheetNames[2]]
-	
-	var rowStart = 18,
-	  	colStart = 28,
-	  	dateRow = 17
+	var indexes = {
+		rowStart: 18,
+		colStart: 28,
+		dateRow: 17,
+		subTotalRow: 60
+	}
 	var projectSizeData = {}
-
+	var stopIndex = getColumnLimit(sheet, indexes.subTotalRow, 3)
+	console.log(stopIndex)
 	while(checkCell(sheet, rowStart, 1, 'v') != 'Subtotal') {
 		var cellValue = checkCell(sheet, rowStart, 1, 'v')
 		//console.log("cell val is " + cellValue)
@@ -29,16 +32,39 @@ var parseExcelSheet = function(b64String) {
 		}
 		rowStart++
 	}
-	console.log(projectSizeData)
+	//console.log(projectSizeData)
 }
 
-function checkCell(sheet, row, col, type) {
+function getCellValue(sheet, row, col, type) {
 	if(sheet[xlsx.utils.encode_cell({r:row,c:col})] != undefined) {
-		console.log("inner val is " + sheet[xlsx.utils.encode_cell({r:row,c:col})][type])
 		return sheet[xlsx.utils.encode_cell({r:row,c:col})][type]
 	} else {
 		return ''
 	}
+}
+
+// Search for n consecutive 0.00's in the 'Subtotal' row
+function getColumnLimit(sheet, subTotalRow, n) {
+	// Verify correct row
+	if(getCellValue(sheet, subTotalRow, 1, 'v') != 'Subtotal') {
+		return 0
+	}
+	var stopIndex
+	var currentCol = 1
+	var done = false
+	var continue_ = true
+	while(!done) {
+		for(var i = currentCol; i < currentCol + n; i++) {
+			continue_ = continue_ && (getCellValue(sheet, totalRow, i, 'v') == '0.00')
+		}
+		if(continue_) {
+			currentCol += n
+		} else {
+			done = true
+			stopIndex = currentCol
+		}
+	}
+	return stopIndex
 }
 
 module.exports.parseExcelSheet = parseExcelSheet
