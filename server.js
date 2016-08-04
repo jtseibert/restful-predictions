ae658e69800d5de952b5e9d591280271fc53bd58
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-04 09:23:11 -0500

    begin parsing of sheet

--- server.js
+++ server.js
@@ -284,7 +284,7 @@ router.route('/importProjectSize')
   		var rowStart = 18
   		var colStart = 28
   		var projectSizeData = {}
-  		/*(while(sheet[xlsx.utils.encode_cell({r:rowStart,c:1})].v != 'Subtotal') {
+  		while(sheet[xlsx.utils.encode_cell({r:rowStart,c:1})].v != 'Subtotal') {
   			var cellValue = sheet[xlsx.utils.encode_cell({r:rowStart,c:1})].v
   			if(cellValue != '') {
   				projectSizeData[cellValue] = {}
@@ -297,7 +297,7 @@ router.route('/importProjectSize')
   			rowStart++
   		}
   		console.log(projectSizeData)
-		*/
+		
 
 
 		//var json = xlsx.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[2]])
