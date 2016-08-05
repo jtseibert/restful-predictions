ba64a55a1fb81a6e229c1b4e74c3de6b44a2e536
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-05 11:41:25 -0500

    paren

--- models/xlsxHandler.js
+++ models/xlsxHandler.js
@@ -40,7 +40,7 @@ function isInDatabase(opportunityName) {
 	utilities.query(
 		"SELECT EXISTS (SELECT opportunity FROM sales_pipeline WHERE opportunity=$1)",
 		[opportunityName],
-		function(results) {console.log results
+		function(results) {console.log( results)
 			return results.exists}
 	)
 }
