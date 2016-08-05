f8fc6ffbd5063f29897bb3be553344099cfd22fe
Jacob Seibert <12jtseibert@gmail.com>
2016-08-05 09:41:03 -0500

    toString

--- models/utilities.js
+++ models/utilities.js
@@ -86,7 +86,7 @@ function getOpportunities_DB(callback){
 function purgeSalesPipeline_DB(callback){
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		if (err) return process.nextTick(function(){callback(err)})
-		var today = moment(new Date()).format("L")
+		var today = moment(new Date()).format("L").toString()
 		client.query("DELETE FROM sales_pipeline WHERE protected=FALSE OR start_date<"+today, function(err, success){
 			if(err)
 				console.log(err)
