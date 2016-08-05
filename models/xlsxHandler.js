d934ad0f6b27e50f9fa696cbda564829c384a6a8
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-05 11:28:34 -0500

    test querya again

--- models/xlsxHandler.js
+++ models/xlsxHandler.js
@@ -33,7 +33,7 @@ var updateOpportunity = function(opportunityData, callback) {
 * @returns true or false
 */
 function isInDatabase(opportunityName) {
-	return utilities.query("SELECT EXISTS (SELECT opportunity FROM sales_pipeline WHERE opportunity=$1", [opportunityName])
+	return utilities.query("SELECT EXISTS (SELECT opportunity FROM sales_pipeline WHERE opportunity=$1)", [opportunityName])
 }
 
 module.exports.updateOpportunity = updateOpportunity
