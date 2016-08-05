00fef3da1a3fe1e2a5bffbf7ebb2e736cfa36774
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-05 11:22:09 -0500

    testing new query

--- models/xlsxHandler.js
+++ models/xlsxHandler.js
@@ -11,7 +11,9 @@ var utilities = require('./utilities')
 * @param callback - callback to handle status
 */
 var updateOpportunity = function(opportunityData, callback) {
-
+	if(isInDatabase(opportunityData.opportunityName)) {
+		console.log(utilities.query("select stage from sales_pipeline where opportunity=$1", opportunityData.opportunityName))
+	}
 
 
 
@@ -31,10 +33,7 @@ var updateOpportunity = function(opportunityData, callback) {
 * @returns true or false
 */
 function isInDatabase(opportunityName) {
-
-
-
-
+	return utilities.query("SELECT EXISTS (SELECT opportunity FROM sales_pipeline WHERE opportunity=$1", opportunityName)
 }
 
 module.exports.updateOpportunity = updateOpportunity
