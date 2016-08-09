243c17fcc53a88dcb0d81b75c6b9df9c69e0ebcf
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-09 15:07:24 -0500

    another

--- src/xlsxHandler.js
+++ src/xlsxHandler.js
@@ -13,7 +13,6 @@ var async     = require('async')
 */
 var updateDatabase = function(opportunityData, callback) {
 	databaseCheck(opportunityData.opportunityName, function(inDatabase) {
-		console.log(inDatabase)
 		if(inDatabase) {
 			deleteOpportunity(opportunityData.opportunityName, function() {
 				updateOpportunity(opportunityData, function(status) {
@@ -40,6 +39,7 @@ function updateOpportunity(opportunityData, callback) {
 	async.eachOfSeries(sheetData, function insertRole(role, roleKey, callback) {
 		async.eachSeries(role, function(weekAllocations, callback){
 			// Make a new row for every week in the weekAllocations
+			console.log('made it befroe change')
 			async.eachOfSeries(role.weekAllocations, function(allocation, week, callback) {	
 				helpers.query(
 					"INSERT INTO sales_pipeline(opportunity, role, week, allocation, protected) values($1, $2, $3, $4, $5)",
