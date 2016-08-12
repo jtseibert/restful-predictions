2830f07434e98e5c0fff9d53cde49e3f0ebc9b11
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-12 11:50:36 -0500

    nt

--- src/helpers.js
+++ src/helpers.js
@@ -19,34 +19,34 @@ pg.defaults.poolSize = 10
 function query(query, values, callback) {
 	q = query
 	v = values
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	pg.connect(process.env.DATABASE_URL, function pgConnectCallback(err, client, done) {
 		//console.log("query is: " + q + ' with values ' + v)
 		var query
 		if(v != null) {
-			query = client.query(q, v, function(error) {
+			query = client.query(q, v, function queryCallback(error) {
 				if(error) {
 					done()
 					callback(error)
 				} else {
-					query.on("row", function(row, result) {
+					query.on("row", function onRowCallback(row, result) {
 					result.addRow(row)
 					})
-					query.on("end", function(result) {
+					query.on("end", function onEndCallback(result) {
 					done()
 					callback(result.rows)
 					})	
 				}
 			})
 		} else {
-			query = client.query(q, function(error) {
+			query = client.query(q, function queryCallback(error) {
 				if(error) {
 					done()
 					callback(error)
 				} else {
-					query.on("row", function(row, result) {
+					query.on("row", function onRowCallback(row, result) {
 					result.addRow(row)
 					})
-					query.on("end", function(result) {
+					query.on("end", function onEndCallback(result) {
 					done()
 					callback(result.rows)
 					})	
@@ -143,7 +143,7 @@ function opportunityCheck(opportunityName, callback) {
 		"SELECT EXISTS (SELECT opportunity FROM sales_pipeline WHERE opportunity=$1)",
 		[opportunityName],
 		function queryCallback(results) {
-			callback(results[0].exists)
+			process.nextTick(function() {callback(results[0].exists)}
 		}
 	)
 }
