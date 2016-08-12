53759c19e3f2f4bcb938fdc547f5d4a276664e85
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-12 11:29:46 -0500

    print the name

--- src/pipeline.js
+++ src/pipeline.js
@@ -66,7 +66,7 @@ module.exports.syncPipelineWithSalesforce = syncPipelineWithSalesforce
 */
 function syncRows(row, callback) {
 	var curRow = row
-	if(curRow[indexes.OPPORTUNITY_NAME] != '') {
+	console.log('anme is ' + curRow[indexes.OPPORTUNITY_NAME])
 		helpers.opportunityCheck([curRow[indexes.OPPORTUNITY_NAME]], function opportunityCheckCallback(exists) {
 			if(exists) {
 				updateProtectedOpportunity(curRow, function updateProtectedOpportunityCallback() {
@@ -78,9 +78,7 @@ function syncRows(row, callback) {
 				})
 			}
 		})
-	} else {
-		callback(null)
-	}
+	
 }
 //*************************************
 
