f1350fce164b10e4b56bf846f6a0b8219290c5c0
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-12 11:41:44 -0500

    print row, test undefined

--- src/pipeline.js
+++ src/pipeline.js
@@ -66,8 +66,8 @@ module.exports.syncPipelineWithSalesforce = syncPipelineWithSalesforce
 */
 function syncRows(row, callback) {
 	var curRow = row
-	console.log('anme is ' + curRow[indexes.OPPORTUNITY_NAME])
-	if(curRow[indexes.OPPORTUNITY_NAME] != null || curRow[indexes.OPPORTUNITY_NAME != ""]) {
+	console.log('row is ' + curRow)
+	if(curRow[indexes.OPPORTUNITY_NAME] != undefined) {
 		helpers.opportunityCheck(curRow[indexes.OPPORTUNITY_NAME], function opportunityCheckCallback(exists) {
 			if(exists) {
 				updateProtectedOpportunity(curRow, function updateProtectedOpportunityCallback() {
