fc1d621f95b3d2ec12442d946f720a89350ae7d4
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-12 12:08:46 -0500

    remove pg name

--- src/helpers.js
+++ src/helpers.js
@@ -19,7 +19,7 @@ pg.defaults.poolSize = 10
 function query(query, values, callback) {
 	q = query
 	v = values
-	pg.connect(process.env.DATABASE_URL, function pgConnectCallback(err, client, done) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		//console.log("query is: " + q + ' with values ' + v)
 		var query
 		if(v != null) {
