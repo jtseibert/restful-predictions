9c2d7d4badf9bace5c3a5343c51b5abb55a402f0
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-15 15:41:55 -0500

    print temp

--- src/pipeline.js
+++ src/pipeline.js
@@ -241,6 +241,7 @@ var exportToSheets = function(callback) {
 						hours
 					]
 					values.push(temp)
+					console.log('temp is ' + temp)
 					process.nextTick(function() {callback(null)})
 				},
 				function() {
