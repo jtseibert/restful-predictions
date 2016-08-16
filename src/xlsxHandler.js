db1f57608f56690a8888ded2b905eccd97ce93f7
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-16 13:41:42 -0500

    more logs

--- src/xlsxHandler.js
+++ src/xlsxHandler.js
@@ -41,8 +41,6 @@ module.exports.updateDatabaseFromXlsx = updateDatabaseFromXlsx
 function updateOpportunityFromXlsx(opportunityData, callback) {
 	var sheetData = opportunityData.sheetData
 	var opportunityName = opportunityData.opportunityName
-	console.log(sheetData)
-	console.log(opportunityData)
 	async.eachOfSeries(sheetData, function insertRole(week_allocations, roleKey, callback) {
 		// Insert a new row for every role with the week_allocations JSON object
 		helpers.query(
@@ -52,7 +50,6 @@ function updateOpportunityFromXlsx(opportunityData, callback) {
 		)
 	}, 
 	function() {
-		console.log('in the cb')
 		callback()
 	})
 }
