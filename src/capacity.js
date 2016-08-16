6464a0a6b34b6a81dcae8432de9524ca5c055542
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-16 14:33:39 -0500

    record prefix

--- src/capacity.js
+++ src/capacity.js
@@ -17,9 +17,8 @@ var async = require('async')
 var queryCapacity = function(accessToken, path, callback) {
 	var sf = require('node-salesforce')
 	var moment = require('moment')
-	var async = require('async')
 	var capacityData = []
-	
+
 	// Connect to SF
 	var conn = new sf.Connection({
 	  instanceUrl: "https://" + path,
@@ -34,7 +33,7 @@ var queryCapacity = function(accessToken, path, callback) {
     		record.pse__Resource_Role__c,
 			record.Name,
 			record.pse__Utilization_Target__c/100,
-			pse__Utilization_Target__c*40
+			record.pse__Utilization_Target__c*40
 		)
     	capacityData.push(recordData)
 		})	
