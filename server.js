e97a07d3c88923b70edf8f8690ebad234302c960
Sheldon Tan <sheldon.tan@magnet360.com>
2016-08-16 12:30:38 -0500

    fix the name param in add op

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ router.route('/updatePipelineTable')
 		switch(req.body.type) {
 			case "add":
 				pipeline.insertWithDefaultSize(req.body.opportunityData, function callback() {
-					helpers.setOpportunityStatus([req.body.opportunityData[1]], req.body.status, function callback() {
+					helpers.setOpportunityStatus([req.body.opportunityName, req.body.status, function callback() {
 						pipeline.exportToSheets(function callback(pipelineData) {
 							res.json(pipelineData)
 						})			
