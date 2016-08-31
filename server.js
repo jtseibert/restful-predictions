29f3832215019745e433df36d93dc6caf038518e
Jacob Seibert <12jtseibert@gmail.com>
2016-08-31 11:57:31 -0500

    changed errorLog calls out of a try catch

--- server.js
+++ server.js
@@ -28,90 +28,90 @@ app.use('/api', router)
 // General query route for Google Sheets to pull from Heroku postgres DB
 router.route('/query')
 	.post(function(req, res) {
-		try {
-			helpers.query(req.body.query, req.body.values, function(error, results) {
-				if (error) { throw error }
+		helpers.query(req.body.query, req.body.values, function(error, results) {
+			if (error) {
+				helpers.errorLog(error)
+				res.json(error)
+			} else {
 				res.json(results)
-			})
-		} catch(error) {
-			helpers.errorLog(error)
-			res.json(error)
-		}
+			}
+		})
 	})
 
 // Get current allocation data from salesforce and export to Google Sheets
 router.route('/:instance/DATA_Allocation/:accessToken')
 	.get(function(req, res) {
-		try{
-			var accessToken = req.params.accessToken,
-				instance    = req.params.instance
-			allocation.queryAllocation(accessToken, instance, function(error, allocationData) {
-				if (error) { throw error }
+		var accessToken = req.params.accessToken,
+			instance    = req.params.instance
+		allocation.queryAllocation(accessToken, instance, function(error, allocationData) {
+			if (error) {
+				helpers.errorLog(error)
+				res.json(error)
+			} else {
 				res.json(allocationData)
-			})
-		} catch(error) {
-			helpers.errorLog(error)
-			res.json(error)
-		}
-		
+			}
+		})
 	})
 	   
 // Get current sales pipeline data from salesforce, update pipeline table, and export to Google Sheets
 router.route('/:instance/DATA_Sales_Pipeline/:accessToken')
 	.get(function(req, res) {
-		try {
-			async.series({
-				one: async.apply(pipeline.syncPipelineWithSalesforce, req.params.accessToken, req.params.instance),
-				two: pipeline.exportToSheets
-			}, function(error, results){
-				if (error) { throw error }
+		async.series({
+			one: async.apply(pipeline.syncPipelineWithSalesforce, req.params.accessToken, req.params.instance),
+			two: pipeline.exportToSheets
+		}, function(error, results){
+			if (error) {
+				helpers.errorLog(error)
+				res.json(error)
+			} else {
 				res.json(results.two)
-			})
-		} catch(error) {
-			helpers.errorLog(error)
-			res.json(error)
-		}
+			}
+		})
 	})
 
 // Get current capacity data from salesforce, update roles_capacities database, and export to Google Sheets
 router.route('/:instance/DATA_Capacity/:accessToken')
 	.get(function(req, res) {
-		try {
-			var accessToken = req.params.accessToken,
-				instance    = req.params.instance
+		var accessToken = req.params.accessToken,
+			instance    = req.params.instance
 
-			async.waterfall([
-				async.apply(capacity.queryCapacity,accessToken,instance),
-				capacity.clearCapacityTable,
-				capacity.insertCapacity,
-				capacity.exportCapacity
-			], function(error, results) {
-				if (error) { throw error }
+		async.waterfall([
+			async.apply(capacity.queryCapacity,accessToken,instance),
+			capacity.clearCapacityTable,
+			capacity.insertCapacity,
+			capacity.exportCapacity
+		], function(error, results) {
+			if (error) { 
+				helpers.errorLog(error)
+				res.json(error)
+			} else {
 				res.json(results)
-			})
-		} catch(error){
-			helpers.errorLog(error)
-			res.json(error)
-		}
+			}
+		})
 	})
 
 // Create forecast data from allocation/pipeline data (@param req.body) and roles_capacities table,
 // and export to Google Sheets
 router.route('/DATA_Forecast')
 	.post(function(req, res) {
-		try {
-			forecast = new Forecast(req.body, function(error) {
-				if (error) { throw error }
+		forecast = new Forecast(req.body, function(error) {
+			if (error) {
+				helpers.errorLog(error)
+				res.json(error)
+				delete forecast
+			} else {
 				forecast.create(function(error) {
-					if (error) { throw error }
-					res.json(forecast.returnData)
-					delete forecast
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+						delete forecast
+					} else {
+						res.json(forecast.returnData)
+						delete forecast
+					}
 				})
-			})
-		} catch(error) {
-			helpers.errorLog(error)
-			res.json(error)
-		}
+			}
+		})
 	})
 
 // Main route for Google Sheet buttons
@@ -119,105 +119,124 @@ router.route('/DATA_Forecast')
 // Update opportunities in table when project sizes are changed or added
 router.route('/updatePipelineTable')
 	.post(function(req, res) {
-		try {
-			switch(req.body.type) {
-				case "add":
-					async.series({
-						one: async.apply(pipeline.insertWithDefaultSize, req.body.opportunityData),
-						two: async.apply(helpers.setOpportunityStatus,[req.body.opportunityName],req.body.status),
-						three: pipeline.exportToSheets
-					}, function(error, results) {
-						if (error) { throw error }
-						res.json(results.three)
-					})
-					break
-				case "update_generic":
-					async.series({
-						one: async.apply(helpers.query, req.body.query, req.body.values),
-						two: async.apply(pipeline.syncSingleOpportunity, req.body.opportunityName),
-						three: pipeline.exportToSheets
-					}, function(error, results){
-						if (error) { throw error }
-							res.json(results.three)
-					})
-					break
-				case "update_pipeline":
-					async.series({
-						one: async.apply(helpers.query, req.body.query, req.body.values),
-						two: async.apply(helpers.query, "SELECT attachment FROM sales_pipeline where opportunity = $1", [req.body.opportunityName])
-					},function(error, results){
-						if (error) { throw error }
+		switch(req.body.type) {
+			case "add":
+				async.series({
+					one: async.apply(pipeline.insertWithDefaultSize, req.body.opportunityData),
+					two: async.apply(helpers.setOpportunityStatus,[req.body.opportunityName],req.body.status),
+					three: pipeline.exportToSheets
+				}, function(error, results) {
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else { res.json(results.three) }
+				})
+				break
+			case "update_generic":
+				async.series({
+					one: async.apply(helpers.query, req.body.query, req.body.values),
+					two: async.apply(pipeline.syncSingleOpportunity, req.body.opportunityName),
+					three: pipeline.exportToSheets
+				}, function(error, results){
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else { res.json(results.three) }
+				})
+				break
+			case "update_pipeline":
+				async.series({
+					one: async.apply(helpers.query, req.body.query, req.body.values),
+					two: async.apply(helpers.query, "SELECT attachment FROM sales_pipeline where opportunity = $1", [req.body.opportunityName])
+				},function(error, results){
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else {
 						if (results.two[0]) {
 							pipeline.exportToSheets(function(error, pipelineData) {
-								if (error) { throw error }
-								res.json(pipelineData)
+								if (error) {
+									helpers.errorLog(error)
+									res.json(error)
+								} else { res.json(pipelineData) }
 							})
 						} else {
 							async.series({
 								one: async.apply(pipeline.syncSingleOpportunity, req.body.opportunityName),
 								two: pipeline.exportToSheets
 							}, function(error, results){
-								if (error) { throw error }
-								res.json(results.two)
+								if (error) {
+									helpers.errorLog(error)
+									res.json(error)
+								} else { res.json(results.two) }
 							})
 						}
-					})
-					break
-				case "remove":
-					async.series({
-						one: async.apply(helpers.deleteOpportunities, req.body.opportunities),
-						two: pipeline.exportToSheets
-					}, function(error, results){
-						if (error) { throw error }
-						res.json(results.two)
-					})
-					break
-				case "omit":
-					async.series({
-						one: async.apply(helpers.setOpportunityStatus, req.body.opportunities, req.body.status),
-						two: pipeline.exportToSheets
-					}, function(error, results){
-						if (error) { throw error }
-						res.json(results.two)
-					})
-					break
-				case "project_size":
-					async.series({
-						one: async.apply(helpers.query, req.body.query, req.body.values),
-						two: pipeline.syncWithDefaultSizes,
-						three: pipeline.exportToSheets
-					}, function(error, results){
-						if (error) { throw error }
-						res.json(results.three)
-					})
-					break
-				case "assign_role":
-					async.series({
-						one: async.apply(capacity.assignRole, req.body.name, req.body.role),
-						two: capacity.exportCapacity
-					}, function(error, results){
-						if (error) { throw error }
-						res.json(results.two)
-					})
-					break
-				case "debug":
-					pipeline.exportToSheets(function(error, pipelineData) {
-						if (error) { throw error }
-						res.json(pipelineData)
-					})
-					break
-				case "protected":
-					helpers.setOpportunityStatus(req.body.opportunities, req.body.status, function(error) {
-						if (error) { throw error }
-						res.json({message: "Success!"})
-					})
-					break
-				default:
-					res.json({message: "Default case, No Update."})
-			}
-		} catch(error) {
-			helpers.errorLog(error)
-			res.json(error)
+					}
+				})
+				break
+			case "remove":
+				async.series({
+					one: async.apply(helpers.deleteOpportunities, req.body.opportunities),
+					two: pipeline.exportToSheets
+				}, function(error, results){
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else { res.json(results.two) }
+				})
+				break
+			case "omit":
+				async.series({
+					one: async.apply(helpers.setOpportunityStatus, req.body.opportunities, req.body.status),
+					two: pipeline.exportToSheets
+				}, function(error, results){
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else { res.json(results.two) }
+				})
+				break
+			case "project_size":
+				async.series({
+					one: async.apply(helpers.query, req.body.query, req.body.values),
+					two: pipeline.syncWithDefaultSizes,
+					three: pipeline.exportToSheets
+				}, function(error, results){
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else { res.json(results.three) }
+				})
+				break
+			case "assign_role":
+				async.series({
+					one: async.apply(capacity.assignRole, req.body.name, req.body.role),
+					two: capacity.exportCapacity
+				}, function(error, results){
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else { res.json(results.two) }
+				})
+				break
+			case "debug":
+				pipeline.exportToSheets(function(error, pipelineData) {
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					}else { res.json(pipelineData) }
+				})
+				break
+			case "protected":
+				helpers.setOpportunityStatus(req.body.opportunities, req.body.status, function(error) {
+					if (error) {
+						helpers.errorLog(error)
+						res.json(error)
+					} else { res.json({message: "Success!"}) }
+				})
+				break
+			default:
+				res.json({message: "Default case, No Update."})
 		}
 	})
 
@@ -225,18 +244,15 @@ router.route('/updatePipelineTable')
 // an xlsx sheet attached to an opportunity object in salesforce
 router.route('/trigger')
 	.post(function(req, res) {
-		try {
-			async.waterfall([
-				async.apply(parser.parseExcelSheet, req.body),
-				xlsxHandler.updateDatabaseFromXlsx
-			], function(error, results){
-				if (error) { throw error }
-				res.json({ message: 'Trigger Done' })
-			})
-		} catch(error) {
-			helpers.errorLog(error)
-			res.json(error)
-		}
+		async.waterfall([
+			async.apply(parser.parseExcelSheet, req.body),
+			xlsxHandler.updateDatabaseFromXlsx
+		], function(error, results){
+			if (error) {
+				helpers.errorLog(error)
+				res.json(error)
+			} else { res.json({ message: 'Trigger Done' }) }
+		})
 	})
 
 // Start server
