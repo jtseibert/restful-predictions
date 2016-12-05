8a5f891e6dd8462fe312fcb56967d8a40a679488
Jacob Seibert <12jtseibert@gmail.com>
2016-12-04 20:24:17 -0600

    typo

--- src/parser.js
+++ src/parser.js
@@ -79,7 +79,7 @@ var parseExcelSheet = function(body, callback) {
 							if (error) { process.nextTick(function(){ callback(error, undefined) }) }
 							else {
 								// Indexes were found, set and proceed
-								console.log('results.one: '+results.one)
+								console.log('results.three: '+results.three)
 								colEnd = results.one
 								year = results.two
 								indexes.dataColStart = results.three
@@ -109,6 +109,7 @@ var parseExcelSheet = function(body, callback) {
 											// Get all week allocations from the columns in the spreadsheet
 
 											console.log('colEnd: '+colEnd)
+											console.log('colStart'+indexes.dataColStart)
 											console.log('number of cols: '+colEnd-indexes.dataColStart)
 
 											async.times(colEnd-indexes.dataColStart, function(n, next){
